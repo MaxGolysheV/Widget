8020f99e1984af0f8a6d7de9ace4df95620ee651
MaxGolyshev <isip_m.a.golyshev@mpt.ru>
2022-06-20 23:07:11 +0300

    first commit

---
+++ app/src/main/java/com/example/widget/Data.java
@@ -0,0 +1,27 @@
+package com.example.widget;
+
+import android.graphics.Bitmap;
+
+public class Data {
+
+    public static Data inst = new Data();
+
+    private String text;
+    private Bitmap img;
+
+    public void setData(String t, Bitmap i)
+    {
+        text = t;
+        img = i;
+    }
+
+    public String getText()
+    {
+        return text;
+    }
+
+    public Bitmap getImg()
+    {
+        return img;
+    }
+}
